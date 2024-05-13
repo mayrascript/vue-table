2b9fdfd6708a4f2a46ee198dd7fce3478b094f98
Mayra Rodriguez <mayrarodriguez1709@gmail.com>
2024-05-12 22:03:26 -0500

    feat: TaskPane base component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app">
     <DealGrid />
+    <TaskPane />
   </div>
 </template>
 
 <script>
 import DealGrid from './components/DealGrid.vue';
 import { provide, ref } from 'vue';
+import TaskPane from '@/components/TaskPane.vue'
 
 export default {
   components: {
+    TaskPane,
     DealGrid,
   },
   setup() {
---
+++ src/components/TaskPane.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="{ 'task-pane-open': isOpen }" class="task-pane">
+    <div class="content">
+    </div>
+    <button @click="closeTaskPane" class="close-button">Close</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const isOpen = ref(true);
+
+const openTaskPane = () => {
+  isOpen.value = true;
+};
+
+const closeTaskPane = () => {
+  isOpen.value = false;
+};
+</script>
+
+<style lang="scss">
+.task-pane {
+  width: 300px;
+  position: fixed;
+  top: 0;
+  right: -300px;
+  transition: right 0.3s ease;
+  outline: 1px solid red;
+  height: 100%;
+  background: white;
+  padding: 16px;
+
+  &.task-pane-open {
+    right: 0;
+  }
+
+  .content {
+  }
+
+  .close-button {
+    }
+}
+</style>
